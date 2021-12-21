5f6098f9c4f66017cebc189c01643723e8a18791
Pritam <sritam.mallick@gmail.com>
2021-12-21 22:01:08 +0530

    Comic 4 added

--- src/data/comiclist.js
+++ src/data/comiclist.js
@@ -1,6 +1,7 @@
 import c1 from '../home/images/c1.png';
 import c2 from '../home/images/c2.png';
 import c3 from '../home/images/c3.png';
+import c4 from '../home/images/c4.png';
 
 export const comicVars = [
     {
@@ -16,6 +17,11 @@ export const comicVars = [
     {
       name: "Comic 3",
       image: c3,
+      desc: "Description",
+    },
+    {
+      name: "Comic 4",
+      image: c4,
       desc: "Description",
     }
 ];
